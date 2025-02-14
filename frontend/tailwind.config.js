8c5709973f63c2830726b1313263799f0c3a1519
thanattouth <thanattouth.nit@dome.tu.ac.th>
2025-02-14 23:09:17 +0700

    start-project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",               // HTML files
+    "./src/**/*.{js,jsx,ts,tsx}",  // React/JSX/TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
