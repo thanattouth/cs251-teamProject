afe0868433417d60842f9bdb565e4eb088986009
Suphawit Hngosakul - 6609650053 <145352874+Suphawit-Hngosakul@users.noreply.github.com>
2025-02-15 00:01:53 +0700

    config-start-project

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from Backend!" });
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
+  plugins: [react(), tailwindcss()],
+  server: {port: 3000},
 })
